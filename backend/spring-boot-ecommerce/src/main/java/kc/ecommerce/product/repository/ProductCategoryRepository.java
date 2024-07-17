e61d8a049a6ac0694d1cc303640ec639371a1cb9
Krzysztof Caban <kcaban99@gmail.com>
2024-07-17 17:14:37 +0200

    Added Cross origin support

--- backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/repository/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package kc.ecommerce.product.repository;
 import kc.ecommerce.product.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin({"http://localhost:4200", "http://frontend-ecommerce:80"})
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/repository/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package kc.ecommerce.product.repository;
 import kc.ecommerce.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin({"http://localhost:4200", "http://frontend-ecommerce:80"})
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
