a90e204c25f29058c9db13e1c2e040d2db2d1ba7
Krzysztof Caban <kcaban99@gmail.com>
2024-07-17 14:46:33 +0200

    Init backend, create models and configure data rest, added Dockerfile

---
+++ backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/repository/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package kc.ecommerce.product.repository;
+
+import kc.ecommerce.product.model.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
