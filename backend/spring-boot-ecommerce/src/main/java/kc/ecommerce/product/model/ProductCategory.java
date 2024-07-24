6aeb83ad56ad4a13b2abb91ca1deb9314a13e0da
Krzysztof Caban <kcaban99@gmail.com>
2024-07-24 18:14:24 +0200

    changed ProductCategory name field

--- backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/model/ProductCategory.java
+++ backend/spring-boot-ecommerce/src/main/java/kc/ecommerce/product/model/ProductCategory.java
@@ -16,7 +16,7 @@ public class ProductCategory {
     @Column(name = "id")
     private Long id;
     @Column(name = "category_name")
-    private String category;
+    private String categoryName;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
 }
